8c91bd40be79e5b0aaca4fbc6c70ddf444805efd
Victor Shepardson <victor.shepardson@gmail.com>
2022-03-03 17:15:53 +0000

    time in osc app

--- pytorch-osc/pytorch-osc.py
+++ pytorch-osc/pytorch-osc.py
@@ -7,7 +7,7 @@ Authors:
 
 import asyncio
 import fire
-from notepredictor import PitchPredictor
+from notepredictor import NotePredictor
 from osc import OSC
 
 def predictor_handler(address, *args):
@@ -19,14 +19,14 @@ def predictor_handler(address, *args):
     if(address[2] == "load"):
         print(f"/load {args}")
         global predictor
-        predictor = PitchPredictor.from_checkpoint(*args)
+        predictor = NotePredictor.from_checkpoint(*args)
         predictor.eval()
 
     elif(address[2] == "predict"):
         print(f"/predict {args}")
         r = predictor.predict(*args) # sanity check args
         print(r)
-        osc.send_message('/prediction', (r,))
+        osc.send_message('/prediction', (r['pitch'], r['time']))
 
     elif(address[2] == "reset"):
         print(f"/reset {args}")
@@ -60,7 +60,7 @@ def main(ip="127.0.0.1", send=57120, receive=9999, checkpoint=None):
     osc = OSC(ip, send, receive)
 
     if checkpoint is not None:
-        predictor = PitchPredictor.from_checkpoint(checkpoint)
+        predictor = NotePredictor.from_checkpoint(checkpoint)
         predictor.eval()
 
     asyncio.run(init_main())
