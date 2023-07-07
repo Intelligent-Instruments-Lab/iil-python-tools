088ab2dbbc23269dd2dd1b36120c23cbd31447f4
Jack Armitage <jack.armitage@me.com>
2023-07-07 13:55:39 +0000

    abstract out taichi init

--- tolvera/tolvera/utils.py
+++ tolvera/tolvera/utils.py
@@ -1,4 +1,21 @@
 from iipyper import repeat
+import time
+import taichi as ti
+
+def init(gpu='vulkan', cpu=None):
+    seed = int(time.time())
+    if cpu:
+        ti.init(arch=ti.cpu, random_seed=seed)
+        print("Tolvera running on cpu")
+    else: 
+        match gpu:
+            case "vulkan":
+                ti.init(arch=ti.vulkan, random_seed=seed)
+            case "cuda":
+                ti.init(arch=ti.cuda, random_seed=seed)
+            case _:
+                assert False, f"Invalid GPU: {gpu}"
+        print(f"Tolvera running on {gpu}")
 
 def headless(r, rate=1/60):
     '''
