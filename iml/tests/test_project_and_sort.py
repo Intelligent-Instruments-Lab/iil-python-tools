9ce127cd909e30d7c1cb28358318e41526a1b9bc
Jack Armitage <jack.armitage@me.com>
2023-08-03 14:11:13 +0000

    Use test fixture

--- iml/tests/test_project_and_sort.py
+++ iml/tests/test_project_and_sort.py
@@ -2,23 +2,28 @@ import pytest
 from iml import IML
 import torch
 
-def test_project_and_sort():
+@pytest.fixture
+def setup_iml():
     src_x=16
     src_y=2
     tgt_size=4
 
     d_src = (src_x,src_y)
-    d_tgt = torch.zeros(tgt_size)
     ctrl = torch.rand(d_src)
     z = torch.zeros(tgt_size)
     iml = IML(d_src, embed='ProjectAndSort')
+
     def iml_map():
         while(len(iml.pairs) < 32):
             src = torch.rand(d_src)
             tgt = z + torch.randn(tgt_size)*2
             iml.add(src, tgt)
     iml_map()
-    
+
+    return iml, ctrl, z, tgt_size
+
+def test_update_pos(setup_iml):
+    iml, ctrl, z, tgt_size = setup_iml
     _z = torch.zeros(tgt_size)
     _z[:] = torch.from_numpy(iml.map(ctrl, k=5))
     indices = torch.randperm(ctrl.shape[0])
@@ -28,8 +33,7 @@ def test_project_and_sort():
         indices = torch.randperm(ctrl.shape[0])
         ctrl = ctrl[indices]
         z[:] = torch.from_numpy(iml.map(ctrl, k=5))
- 
+
     for _ in range(32):
         update_pos()
-        print(f"ctrl indices: {indices}, z: {z}, _z: {_z}")
-        assert torch.equal(z, _z), "z should not change, changed by {}".format(z-_z)
+        assert torch.equal(z, _z), f"Expected z to remain constant, but found difference: {z-_z}"
