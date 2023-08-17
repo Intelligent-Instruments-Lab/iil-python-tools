3f09d995a6dc6b9cfb291f0fcacbf3089421a4fb
Jack Armitage <jack.armitage@me.com>
2023-08-17 14:58:43 +0000

    iipyper OSCMap export XML and JSON

--- examples/iipyper/iipyper-oscmap.py
+++ examples/iipyper/iipyper-oscmap.py
@@ -1,7 +1,7 @@
 """
 This example demonstrates how to use the OSCMap class to map OSC messages to Python functions and vice versa.
 
-When this example is run, a Pd patch is created in the background.
+When this example is run, a Pd patch is created in the background, and XML and JSON versions are also saved.
 When the Pd patch is opened, it will send and receive messages to this iipyper server.
 It is possible to output a Max patch instead by altering the command-line arguments.
 """
@@ -22,23 +22,26 @@ def main(host="127.0.0.1", client="127.0.0.1", receive_port=5001, send_port=5000
     client_name = "iipyper-example"
     osc.create_client(client_name, client, send_port)
     
+    '''
+    OSC Map
+
+    Then we create an OSCMap instance:
+    - 'export' is None by default and can be 'JSON', 'XML' or True (for both)
+    '''
+    osc_map = OSCMap(osc, client_name, patch_type, patch_name, pd_bela=pd_bela, export=True)
+
     '''''''''''''''''''''''''''''''''''''''''''''
     OSC Mapping
 
-    Then, we create an OSCMap instance.
     This simple example OSCMap shows one send function and one receive function.
     We add functions to the OSCMap instance using the `add` method.
     In the `@osc_map.add()` decorator we specify OSC parameters as `name=(default, min, max)`.
-    The additional variables `io`, `count`, and `send_mode` describe differnet behaviours:
+    The additional variables `io`, `count`, and `send_mode` describe different behaviours:
     - `io` can be `send` or `receive`
     - `count` is the number of times the function is called before sending or receiving
     - `send_mode` can be `broadcast` or `event`, describing whether the message is sent continuously, or only when called manually.
     We also use type hints in the method signature for sends and receives, and also return type hints for receives, which should always be a tuple.
     '''''''''''''''''''''''''''''''''''''''''''''
-    patch_dir = "examples/iipyper/" if patch_type=="Pd" else "max"
-    appendix = "_local" if host=="127.0.0.1" else "_remote"
-    patch_filepath = patch_dir+'/'+patch_name+appendix
-    osc_map = OSCMap(osc, client_name, patch_type, patch_filepath, pd_bela=pd_bela)
 
     '''
     Patcher → Python
--- iipyper/iipyper/map.py
+++ iipyper/iipyper/map.py
@@ -1,14 +1,18 @@
 '''
 TODO: send args? maybe this is only useful for sending repeated things with fixed args.
+TODO: Make it easier to send non-kwarg lists
+TODO: Refactor self.dict[io][name][params] to a labelled dictionary & add type hints
 '''
 
 from .osc import OSCSendUpdater, OSCSend, OSCReceiveUpdater
 from .max import MaxPatcher
 from .pd import PdPatcher
 
-from typing import Any
+from typing import Any, get_type_hints
 import os
 import sys
+import xml.etree.ElementTree as ET
+import json
 
 class OSCMap:
     '''
@@ -23,14 +27,108 @@ class OSCMap:
                  patch_filepath="osc_controls",
                  create_patch=True,
                  pd_net_or_udp="udp",
-                 pd_bela=False) -> None:
+                 pd_bela=False,
+                 export=None # 'JSON' | 'XML' | True
+                 ) -> None:
         self.osc = osc
         self.client_name = client_name
         self.client_address, self.client_port = self.osc.client_names[self.client_name]
         self.dict = {'send': {}, 'receive': {}}
         self.create_patch = create_patch
+        self.patch_filepath = patch_filepath
+        self.patch_type = patch_type
         if create_patch is True:
             self.init_patcher(patch_type, patch_filepath, pd_net_or_udp, pd_bela)
+        if export is not None:
+            assert export == 'JSON' or export == 'XML' or export == True, "export must be 'JSON', 'XML' or True"
+        self.export = export
+
+    def export_dict(self):
+        '''
+        Save the OSCMap dict as XML
+        '''
+        client_ip, client_port = self.osc.client_names[self.client_name]
+        metadata = {
+            'HostIP': self.osc.host,
+            'HostPort': str(self.osc.port),
+            'ClientName': self.client_name,
+            'ClientIP': client_ip,
+            'ClientPort': str(client_port)
+        }
+        root = ET.Element("OpenSoundControlSchema")
+        metadata_element = ET.SubElement(root, "Metadata", **metadata)
+        sends = self.dict['send']
+        receives = self.dict['receive']
+        for io in ['Send', 'Receive']:
+            ET.SubElement(root, io)
+        for io in ['send', 'receive']:
+            for name in self.dict[io]:
+                f = self.dict[io][name]
+                kw = {
+                    "Address": '/'+name.replace('_', '/'),
+                    "Params": str(len(f['params']))
+                }
+                route = ET.SubElement(root.find(io.capitalize()), "Route", **kw)
+                for i, param in enumerate(f['params']):
+                    kw = {
+                        "Name": param,
+                        "Type": f['hints'][param].__name__,
+                        "Default": str(f['params'][param][0]),
+                        "Min": str(f['params'][param][1]),
+                        "Max": str(f['params'][param][2])
+                    }
+                    ET.SubElement(route, "Param", **kw)
+        tree = ET.ElementTree(root)
+        ET.indent(tree, space="\t", level=0)
+        if self.export == 'XML':
+            self.save_xml(tree, root)
+        elif self.export == 'JSON':
+            self.save_json(root)
+        elif self.export == True:
+            self.save_xml(tree, root)
+            self.save_json(root)
+
+    def save_xml(self, tree, root):
+        tree.write(self.patch_filepath+".xml")
+        print(f"Exported OSCMap to {self.patch_filepath}.xml")
+
+    def save_json(self, xml_root):
+        json_dict = self.xml_to_json(ET.tostring(xml_root, encoding='utf8', method='xml'))
+        with open(self.patch_filepath+".json", 'w') as f:
+            f.write(json_dict)
+        print(f"Exported OSCMap to {self.patch_filepath}.json")
+
+
+    def pascal_to_camel(self, pascal_str):
+        return pascal_str[0].lower() + pascal_str[1:]
+
+    def etree_to_dict(self, t):
+        tag = self.pascal_to_camel(t.tag)
+        d = {tag: {} if t.attrib else None}
+        children = list(t)
+        if children:
+            dd = {}
+            for dc in map(self.etree_to_dict, children):
+                for k, v in dc.items():
+                    try:
+                        dd[k].append(v)
+                    except KeyError:
+                        dd[k] = [v]
+            d = {tag: {k: v[0] if len(v) == 1 else v for k, v in dd.items()}}
+        if t.attrib:
+            d[tag].update((self.pascal_to_camel(k), v) for k, v in t.attrib.items())
+        if t.text:
+            text = t.text.strip()
+            if children or t.attrib:
+                if text:
+                    d[tag]['#text'] = text
+            else:
+                d[tag] = text
+        return d
+
+    def xml_to_json(self, xml_str):
+        e = ET.ElementTree(ET.fromstring(xml_str))
+        return json.dumps(self.etree_to_dict(e.getroot()), indent=4)
 
     def init_patcher(self, patch_type, patch_filepath, pd_net_or_udp, pd_bela):
         # create self.patch_dir if it doesn't exist
@@ -50,13 +148,13 @@ class OSCMap:
         else:
             assert False, "`patch_type` must be 'Max' or 'Pd'"
 
-
     def add_func_to_osc_map(self, func, kwargs):
         n = func.__name__
         address = '/'+n.replace('_', '/')
         params = {k: v for k, v in kwargs.items() \
                     if k != 'io' and k != 'count' and k != 'send_mode'}
-        f = {'f': func, 'address': address, 'params': params}
+        hints = get_type_hints(func)
+        f = {'f': func, 'address': address, 'params': params, 'hints': hints}
         if 'io' not in kwargs:
             raise ValueError(f'io must be specified for {n}')
         if 'count' not in kwargs:
@@ -72,6 +170,8 @@ class OSCMap:
             self.dict['receive'][n] = f
         else:
             raise ValueError('io must be send or receive')
+        if self.export is not None:
+            self.export_dict()
         return f
 
     def add_func_to_patcher(self, func, io):
