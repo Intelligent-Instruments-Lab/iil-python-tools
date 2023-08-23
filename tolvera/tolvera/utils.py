c66b7d8a7f482547a7ee133aa52e325c692802ab
Jack Armitage <jack.armitage@me.com>
2023-08-23 14:16:15 +0000

    added cleanup to Tolvera

--- tolvera/tolvera/utils.py
+++ tolvera/tolvera/utils.py
@@ -1,10 +1,10 @@
-from iipyper import repeat
 import time
 import taichi as ti
 from iipyper.state import _lock
 
 '''
 Globals
+TODO: better pattern needed here
 '''
 gpu = 'vulkan'
 cpu = None
@@ -84,16 +84,41 @@ def init(**kwargs):
     window = ti.ui.Window(name, (x,y), fps_limit=fps, show_window=not headless)
     canvas = window.get_canvas()
 
+_cleanup_fns = []
+# decorator to make a function run on KeyBoardInterrupt (before exit)
+def cleanup(f=None):
+    """
+    @cleanup decorator based on iipyper
+    cleanup functions must be defined before render is called!
+    """
+    def decorator(f):
+        _cleanup_fns.append(f)
+        return f
+
+    if f is None: # return a decorator
+        return decorator
+    else: #bare decorator case; return decorated function
+        return decorator(f)
+
 def show(px):
     global window, canvas, headless
     canvas.set_image(px.px.rgba)
     if not headless: window.show()
 
-def render(f=None, px=None):
-    # TODO: Add **kwargs to f()
+def run(f, px):
     global window
+    # TODO: Add **kwargs to f()
     while window.running:
         with _lock:
             if f is not None: f()
             show(px)
 
+def stop():
+    print(f"\nExiting {name}...")
+    for f in _cleanup_fns:
+        f()
+    exit(0)
+
+def render(f=None, px=None):
+    try: run(f, px)
+    except KeyboardInterrupt: stop()
