6012feef231d08a9a4bb88a6ada1c9601d1c99f2
Jack Armitage <jack.armitage@me.com>
2023-08-31 13:32:47 +0100

    fixes

--- examples/iml/tolvera-simple.py
+++ examples/iml/tolvera-simple.py
@@ -15,16 +15,16 @@ def main(x=1920, y=1080, n=64, species=5, fps=120,
     
     # Setup Tolvera
     tol.init(x=x, y=y, n=n, species=species, fps=fps, gpu=gpu, host=host, client=client, receive_port=receive_port, send_port=send_port, headless=headless)
-    particles = tol.Particles(x, y, n, species, wall_margin=0)
+    particles = tol.Particles(x, y, n, species)#, wall_margin=0)
     pixels = tol.Pixels(x, y, evaporate=0.95, fps=fps)
     boids = tol.vera.Boids(x, y, species)
 
     # Setup IML
-    d_src = n*2
+    d_src = (n,2)
     d_tgt = torch.zeros(tgt_size)
     ctrl = torch.zeros(d_src)
     z = torch.zeros(tgt_size)
-    iml = IML(d_src, embed='ProjectAndSort')
+    iml = IML(d_src, emb='ProjectAndSort')
     def iml_add():
         while(len(iml.pairs) < 32):
             src = torch.rand(d_src)#/(ctrl.abs()/2+1)
@@ -33,7 +33,7 @@ def main(x=1920, y=1080, n=64, species=5, fps=120,
     iml_add()
     
     def iml_map():
-        ctrl = particles.osc_get_pos_all_2d()
+        ctrl = particles.get_pos_all_2d()
         z[:] = torch.from_numpy(iml.map(ctrl, k=5))
     iml_update = Updater(iml_map, 24)
 
