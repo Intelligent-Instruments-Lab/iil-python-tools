a9979751d58526d529d5ef674f30763555fbb461
Jack Armitage <jack.armitage@me.com>
2023-11-16 19:01:10 +0000

    Refactored the main function to use the Tolvera class and fire module for command line interface.

--- tolvera/tolvera/__main__.py
+++ tolvera/tolvera/__main__.py
@@ -1,66 +1,20 @@
 '''
-TODO: default app with OSC etc.
+TODO: 'sketchbook' run/export scripts?
 '''
 
-import sys
-import taichi as ti
+import fire
 
-from iipyper import run
-
-# from tolvera.vera.simple import main as simple
-# from tolvera.vera.boids import main as flock
-# from tolvera.vera.physarum import main as physarum
-# from tolvera.vera.particle_life import main as particle_life
-# from tolvera.cv import main as cv
-
-# from tolvera.vera._lenia import main as lenia
-# from tolvera.vera._reaction_diffusion import main as rea_diff
-# from tolvera.vera._mrp import main as mrp
-# from tolvera.vera._obstacles import main as obstacles
+from .__init__ import Tolvera
 
 def help():
-    print("""
-    available subcommands:
-        help:     list available subcommands
-        simple:   run Particles & Pixels example
-        boids:    run Boids example
-        physarum: run Physarum example
-        lenia:    run Lenia example
-        particle_life: run ParticleLife example
-        reaction_diffusion: run ReactionDiffusion example
-        obstacles: run Obstacles exmaple
-    """)
+    print('')
 
 def main(**kwargs):
-    print(sys.argv)
-    # try:
-    #     match sys.argv[1]:
-    #         case 'simple':
-    #             simple(**kwargs)
-    #         case 'boids':
-    #             boids(**kwargs)
-    #         case 'physarum':
-    #             physarum(**kwargs)
-    #         case 'particle_life':
-    #             particle_life(**kwargs)
-    #         case 'cv':
-    #             cv(**kwargs)
-    #         # case 'reaction_diffusion':
-    #         #     rea_diff()
-    #         # case 'lenia':
-    #         #     lenia()
-    #         # case 'world':
-    #         #     world()
-    #         # case 'help':
-    #         #     help()
-    #         # case 'mrp':
-    #         #     mrp()
-    #         # case 'obstacles':
-    #         #     mrp()
-    #         case _:
-    #             help()
-    # except IndexError:
-    #     help()
+    tv = Tolvera(**kwargs)
+
+    @tv.render
+    def _():
+        tv.p()
 
 if __name__=='__main__':
-    run(main)
+    fire.Fire(main)
